31172925b17a6979edca91d4c95834edd9591da5
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2019-08-16 13:58:10 +0200

    Update Main.m

--- Main.m
+++ Main.m
@@ -2,10 +2,14 @@ clear ; close all; clc
 
 fprintf('Finding closest centroids.\n\n');
 
+%range of random matrix
+##A = 0
+##B = 10
+##X = A+(B-A)*rand(600,2);
+X = load('dat.txt')
+K = 7; % Number of centroids
+icentroids = centroidinit(X,K,false);
 
-load('data.mat');
-K = 3; % 3 centroids
-icentroids = centroidinit(X,K);
 
 max_iters = 10;
 [centroids,idx] = runkmeans(X,icentroids,max_iters,true);
